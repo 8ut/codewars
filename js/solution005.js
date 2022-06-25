52701758db9be0dce19a3319ad00fcb552962e07
8ut <timkwok808@gmail.com>
2022-06-25 01:50:18 -0700

    solution006 completed

--- js/solution005.js
+++ js/solution005.js
@@ -2,3 +2,20 @@
 //https://www.codewars.com/kata/55a2d7ebe362935a210000b2/javascript
 
 //tough, use the wrong conditionals and didn't reassign variables correctly so kept returning booleans instead of ints.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      
+        let smallest = 0;
+      
+        for (let i = 0; i < args.length; i++){
+            if(args[i] <= args[0]){
+              args[0] = args[i]
+              smallest = args[i]
+            }
+        }
+        return smallest
+    }
+
+
+}
