3355321b2e02d5e5dc97f2a6b3db4c7a1fae63ee
8ut <timkwok808@gmail.com>
2022-06-24 23:54:14 -0700

    solution005 completed, very hard

---
+++ js/solution005.js
@@ -0,0 +1,4 @@
+//8kyu Find the smallest integer in the array 6/24/22
+//https://www.codewars.com/kata/55a2d7ebe362935a210000b2/javascript
+
+//tough, use the wrong conditionals and didn't reassign variables correctly so kept returning booleans instead of ints.
