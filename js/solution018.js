ee8876e37ecec85e874206641e543b5f2b5cc8d9
8ut <timkwok808@gmail.com>
2022-07-05 21:30:55 -0700

    solution018 completed

---
+++ js/solution018.js
@@ -0,0 +1,11 @@
+//8kyu Remove First and Last Character 7/6/22
+//https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+
+function removeChar(str){
+
+    let last = str.length-1
+  
+    return  str =  str.substring(1,last)
+  
+  }
+   
