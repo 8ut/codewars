e4bb933abe76b8cf12f5eaaa5194c592eeb790ac
8ut <timkwok808@gmail.com>
2022-07-05 21:42:28 -0700

    solution016 completed

---
+++ js/solution016.js
@@ -0,0 +1,10 @@
+// 8kyu Twice as old 7/4/22 rework
+//https://www.codewars.com/kata/5b853229cfde412a470000d0/javascript
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let son = sonYearsOld*2-dadYearsOld
+    console.log(son)
+}
+
+
+twiceAsOld(36,7)
