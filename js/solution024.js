6b828282e53c7201778d26b57975b7d0f0691980
8ut <timkwok808@gmail.com>
2022-07-10 17:02:08 -0700

    solution024 completed

--- js/solution024.js
+++ js/solution024.js
@@ -1,6 +1,7 @@
 //8kyu Convert boolean values to strings 'Yes' or 'No'.
 //https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
 
+
 function boolToWord( bool ){
     if(bool == true){
       return "Yes"
