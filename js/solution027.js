d0df5a4f8434101f9420dc74fc2d4d782bcc4b2b
8ut <timkwok808@gmail.com>
2022-07-12 18:48:52 -0700

    solution027 completed

--- js/solution027.js
+++ js/solution027.js
@@ -1,2 +1,17 @@
 //8kyu Counting sheep...
 //https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
+
+
+function countSheeps(arrayOfSheep) {
+  
+    let amount = 0
+
+    for(i=0;i<arrayOfSheep.length;i++){
+    if(arrayOfSheep[i] == true){
+    amount++
+    }
+}
+    
+    return amount
+    
+}
