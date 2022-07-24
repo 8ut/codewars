cdd61acfdbc1c2eb393592edb7c2c1cdb13da5db
8ut <93302447+8ut@users.noreply.github.com>
2022-07-23 17:03:11 -0700

    Add files via upload

---
+++ js/solution024.js
@@ -0,0 +1,12 @@
+//8kyu Convert boolean values to strings 'Yes' or 'No'.
+//https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+//ez
+
+function boolToWord( bool ){
+    if(bool == true){
+      return "Yes"
+    }else{
+      return "No"
+  }
+}
