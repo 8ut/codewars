b04d0bab55068d12a069c447f0f92cbd0f5f87a2
8ut <timkwok808@gmail.com>
2022-07-25 23:54:38 -0700

    solution26 completed leetss gooooooo

--- js/solution026.js
+++ js/solution026.js
@@ -1,3 +1,23 @@
 //7kyu Get the Middle Character
 //https://www.codewars.com/kata/56747fd5cb988479af000028/train/javascript
 
+function getMiddle(s){
+
+let oddEven = Math.floor(s.length/2);
+console.log(oddEven)
+  
+if (s.length % 2 == 0){
+    
+    let even = [s[oddEven-1], s[oddEven]]
+
+    return even.join("")
+
+
+}else if(s.length % 2 == 1){
+    
+    return s[oddEven]
+    
+}
+}
+
+console.log(getMiddle("middle"))
