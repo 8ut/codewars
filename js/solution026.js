4de738337758ae989a4c5f36c83682373da9562d
8ut <timkwok808@gmail.com>
2022-07-26 02:10:50 -0700

    solution027

--- js/solution026.js
+++ js/solution026.js
@@ -3,21 +3,21 @@
 
 function getMiddle(s){
 
-let oddEven = Math.floor(s.length/2);
-console.log(oddEven)
-  
-if (s.length % 2 == 0){
+    let oddEven = Math.floor(s.length/2);
+    console.log(oddEven)
+      
+    if (s.length % 2 == 0){
+        
+        let even = [s[oddEven-1], s[oddEven]]
     
-    let even = [s[oddEven-1], s[oddEven]]
-
-    return even.join("")
-
-
-}else if(s.length % 2 == 1){
+        return even.join("")
     
-    return s[oddEven]
     
-}
-}
+    }else if(s.length % 2 == 1){
+        
+        return s[oddEven]
+        
+    }
+    }
 
 console.log(getMiddle("middle"))
