de7108b67c83ea4684aa0e3eefc3f389b4c84ccf
8ut <timkwok808@gmail.com>
2022-07-30 01:05:18 -0700

    solution029 can't read

--- js/solution029.js
+++ js/solution029.js
@@ -1,3 +1,59 @@
 //8kyu Welcome!
 //https://www.codewars.com/kata/577ff15ad648a14b780000e7/javascript
 
+function greet(language) {
+
+    switch (language) {
+        case 'Welcome':
+            return "english"
+        break;
+        case 'Vitejte':
+            return "czech"
+        break;
+        case 'Velkomst':
+            return "danish"
+        break;
+        case 'Welkom':
+            return "dutch"
+        break;
+        case 'Tere tulemast':
+            return "estonian"
+        break;
+        case 'Tervetuloa':
+            return "finnish"
+        break;
+        case 'Welgekomen':
+            return "flemish"
+        break;
+        case 'Bienvenue':
+            return "french"
+        break;
+        case 'Willkommen':
+            return "german"
+        break;
+        case 'Benvenuto':
+            return "italian"
+        break;
+        case 'Gaidits':
+            return "latvian"
+        break;
+        case 'lithuanian':
+            return 'Laukiamas'
+        break;
+        case 'Witamy':
+            return "polish"
+        break;
+        case 'Bienvenido':
+            return "spanish"
+        break;
+        case 'Valkommen':
+            return "swedish"
+        break;
+        case 'Croeso':
+            return "welsh"
+        break;
+
+
+    }
+	
+}
