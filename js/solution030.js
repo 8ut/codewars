56319b9527f2264d9c3f8f9edc0090ac8b883697
8ut <timkwok808@gmail.com>
2022-07-31 02:09:35 -0700

    solution030 completed first try, solution31 REGULAR EXPRESSIONS

--- js/solution030.js
+++ js/solution030.js
@@ -1,3 +1,23 @@
 //7kyu Categorize New Member
 //https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/javascript
 
+//FIRST TRRRRRRYYYYYYYYYYYY LESSS GOOOOOOOOOOOO
+
+function openOrSenior(data){
+    let output = []
+    for(let i = 0; i<data.length; i++){
+        if (data[i][0] >= 55 && data[i][1] > 7){
+            output[i] = "Senior"
+        }
+        else{
+            output[i] = "Open"
+        }
+        
+    }
+    return output;
+}
+
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))
+
+
+
--- js/solution031.js
+++ js/solution031.js
@@ -1,2 +1,30 @@
 //7kyu Regex validate PIN code
 //https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/train/javascript
+
+function validatePIN (pin) {
+
+    //Regular expressions
+
+    return /^(\d{4}|\d{6})$/.test(pin)
+
+
+    //attempt
+    // if(!Number.isInteger(pin) && pin % 1 != 0){
+    //     return false
+    // }
+
+
+    // else if(pin.length == "4" || pin.length == "6"){
+    //     return true
+        
+    // }else if(pin.length > "4" || pin.length > "6" || pin.length == 5 || pin.length < "4" || pin.length < "6"){
+    //   return false
+    // }
+    //   else{
+    //     return false
+    // }
+
+}
+
+console.log(validatePIN("12345"))
+console.log(validatePIN("1234"))
