0198358b96683123061fcd6ae85a1083a97664d4
8ut <timkwok808@gmail.com>
2022-08-07 21:34:46 -0700

    solution037

--- js/solution036.js
+++ js/solution036.js
@@ -2,5 +2,5 @@
 //https://www.codewars.com/kata/568d0dd208ee69389d000016/train/javascript
 
 function rentalCarCost(d) {
-    // Your solution here
-  }
+    
+}
--- js/solution037.js
+++ js/solution037.js
@@ -1,6 +1,10 @@
 //7kyu Sum of odd numbers
 //https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
 
+//this one is dumb, the solution is way easier than what the problem was seen.
+
 function rowSumOddNumbers(n) {
-	
+    return Math.pow(n, 3)
 }
+
+console.log(rowSumOddNumbers(42))
