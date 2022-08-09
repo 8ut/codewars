3f37e0a51292cf1633444ae0379aa50a7f984527
8ut <timkwok808@gmail.com>
2022-08-08 21:49:38 -0700

    solution038 completed

--- js/solution036.js
+++ js/solution036.js
@@ -2,5 +2,15 @@
 //https://www.codewars.com/kata/568d0dd208ee69389d000016/train/javascript
 
 function rentalCarCost(d) {
-    
+    let rent = d * 40
+
+    if(d>=3 && d<7){
+        rent -= 20
+    }else if(d>=7){
+        rent -= 50
+    }
+
+    return rent
 }
+
+console.log(rentalCarCost(7))
