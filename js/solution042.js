9000a2749ddc86ba77895288ec2cf01f05c776f0
8ut <timkwok808@gmail.com>
2022-08-11 19:02:29 -0700

    solution041 confusion

---
+++ js/solution042.js
@@ -0,0 +1,6 @@
+//7kyu Money, Money, Money
+//https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript
+
+function calculateYears(principal, interest, tax, desired) {
+    // your code
+}
