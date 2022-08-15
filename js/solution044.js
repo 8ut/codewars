60c610d22d33bf11a669f4744b75e9c425bc9412
Tim Kwok <timkwok808@gmail.com>
2022-08-14 23:17:31 -0700

    solution045

--- js/solution044.js
+++ js/solution044.js
@@ -1,1 +1,6 @@
-//bruh why is git like this just let me merge to main
+//7kyu Testing 1-2-3
+//https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
+
+var number=function(array){
+    //your awesome code here
+  }
