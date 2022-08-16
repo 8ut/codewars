2282862c9c31426367ee316b3c4505b427f3a2d4
Tim Kwok <timkwok808@gmail.com>
2022-08-15 21:04:40 -0700

    solution045

---
+++ js/solution045.js
@@ -0,0 +1,7 @@
+//6kyu Count characters in your string
+//https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/javascript
+
+function count (string) {  
+    // The function code should be here
+     return {};
+  }
