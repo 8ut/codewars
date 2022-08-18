fcedda03d35953b63d40c36af45f1a2981dc48d1
8ut <timkwok808@gmail.com>
2022-08-17 23:44:46 -0700

    solution042 taxes

--- js/solution042.js
+++ js/solution042.js
@@ -1,6 +1,28 @@
 //7kyu Money, Money, Money
 //https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript
+//cant seem to get the tax rate right
 
 function calculateYears(principal, interest, tax, desired) {
-    // your code
+
+    let money = principal
+    let year = 0
+    let rate = tax/10
+    let sales = principal*rate
+
+    while(principal < desired){
+    principal = principal * interest
+
+    money = money + principal
+
+    principal = money - sales
+
+    console.log(principal)
+    console.log(sales)
+    sales = principal*rate
+    year++
+    }
+
+    return year
 }
+
+console.log(calculateYears(1000, 0.05, 0.18, 1100))
