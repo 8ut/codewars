d65597fda5cfb023e2494c4b5d3d7090a9dfbcb9
Tim Kwok <timkwok808@gmail.com>
2022-08-23 21:17:10 -0700

    solution044 first try

--- js/solution044.js
+++ js/solution044.js
@@ -2,6 +2,14 @@
 //https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
 
 var number=function(array){
-    //your awesome code here
-  }
+    let arr = array.splice("")
+
+    for(let i = 0; i < arr.length; i++){
+      arr[i] = `${i+1}: ${arr[i]}`
+    }
+    return arr
+
+}
+
+console.log(number(["a", "b", "c"]))
 
