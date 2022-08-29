44a30c199f0830428733425610e2bd404ac873a1
Tim Kwok <timkwok808@gmail.com>
2022-08-28 22:35:36 -0700

    solution045 first 6kyu completed, but what does arr.forEach(function(p) { count[p] = (count[p]||0) + 1;}); mean

--- js/solution045.js
+++ js/solution045.js
@@ -2,6 +2,21 @@
 //https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/javascript
 
 function count (string) {  
-    // The function code should be here
-     return {};
-  }
+
+
+    let arr = []
+    let count = {}
+
+    for(let i = 0; i < string.length; i++){
+    arr[i] = string[i]
+
+    }
+
+    arr.forEach(function(p) { count[p] = (count[p]||0) + 1;});
+
+    return count;
+}
+
+
+
+console.log((count("aba")))
