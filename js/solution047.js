fe073e3ceed8a049a9a8a298f62dd6275789ce98
Tim Kwok <timkwok808@gmail.com>
2022-08-29 19:21:49 -0700

    solution047 ez

--- js/solution047.js
+++ js/solution047.js
@@ -1,2 +1,5 @@
-//8kyu Grasshopper - Messi goals function
+//8kyu Grasshopper - Messi goals function 8/29/22
 //https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
