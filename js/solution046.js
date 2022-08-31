058cfa90ff13ab205efb0f80555df0e26d655a07
Tim Kwok <timkwok808@gmail.com>
2022-08-30 21:20:50 -0700

    solution046 failed understand this line if(word.lastIndexOf(word[i]) == word.indexOf(word[i])){

--- js/solution046.js
+++ js/solution046.js
@@ -1,2 +1,24 @@
-//6kyu Duplicate Encoder
+//6kyu Duplicate Encoder 8/30/22
 //https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
+
+//
+
+//solution
+function duplicateEncode(word){
+    var unique='';
+    word = word.toLowerCase();
+    for(var i=0; i<word.length; i++){
+        console.log(word.lastIndexOf(word[i]) == word.indexOf(word[i]))
+        console.log(word.lastIndexOf(word[i]))
+        console.log(word.indexOf(word[i]))
+        if(word.lastIndexOf(word[i]) == word.indexOf(word[i])){
+            unique += '(';
+        }
+        else{
+            unique += ')';
+        }
+    }
+    return unique;
+}
+
+console.log(duplicateEncode("bruhb"))
