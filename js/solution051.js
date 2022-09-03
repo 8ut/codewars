8824692343989d0baea6d247f8460df029d6cb9a
Tim Kwok <timkwok808@gmail.com>
2022-09-03 11:17:07 -0700

    solution051 ez

---
+++ js/solution051.js
@@ -0,0 +1,12 @@
+//8kyu convert boolean into string 9/3/22
+//https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+function booleanToString(b){
+    let string;
+    if(b == true){
+      string = "true"
+    }else{
+      string = "false"
+    }
+    return string
+}
