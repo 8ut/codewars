feb28439fbd812edd7fae772c6e0cedee2815c64
Tim Kwok <timkwok808@gmail.com>
2022-09-12 23:18:28 -0700

    solution055 ez vowels

--- js/solution055.js
+++ js/solution055.js
@@ -2,7 +2,22 @@
 //https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
 
 function getCount(str) {
-    return 0;
+    arr = str.split("")
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == "a"){
+            count++
+        }else if(arr[i] == "e"){
+            count++
+        }else if(arr[i] == "i"){
+            count++
+        }else if(arr[i] == "o"){
+            count++
+        }else if(arr[i] == "u"){
+            count++
+        }
+    }
+    return count;
 }
 
-getCount("abracadabra");
+console.log(getCount("abracadabra"));
