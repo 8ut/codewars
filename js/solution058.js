2032142618e3871fa80591c553fc71092dfdc3b5
Tim Kwok <timkwok808@gmail.com>
2022-09-15 13:30:14 -0700

    solution058 ez

---
+++ js/solution058.js
@@ -0,0 +1,10 @@
+//8kyu Grasshopper - Check for factor 9/15/22
+//https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/javascript
+
+function checkForFactor (base, factor) {
+    if(base%factor == 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
